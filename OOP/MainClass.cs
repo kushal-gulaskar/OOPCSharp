170b22929870fcde823e9073bc4750b75cef0853
Kushal Gulaskar <kushal.gulaskar@outlook.com>
2018-12-29 13:09:51 +0530

    Array element find and Sting rev

--- OOP/MainClass.cs
+++ OOP/MainClass.cs
@@ -50,6 +50,50 @@ namespace OOP
             inter.SetTutorial(1, "Interface example");
             Console.WriteLine(inter.GetTutorial());
 
+            Console.WriteLine("*************C# Missing Element Array***************");
+
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 9 };
+            //array length
+            int n = arr.Length;
+
+            //sort reverse array
+            Array.Reverse(arr);
+
+            //sort array
+            Array.Sort(arr);
+
+            //Sort and print array
+            foreach (int i in arr)
+            {
+                Console.Write(i + "");
+            }
+
+            //Sum of the array
+            int sum = (n + 1) * (n + 2) / 2;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum = sum - arr[i];
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Missing element is---");
+            Console.WriteLine(sum);
+
+            Console.WriteLine("*************C# string word reverse Element Array***************");
+
+            string str = "", reverse = "";
+            int length = 0;
+            Console.WriteLine("Enter a Word");
+            str = Console.ReadLine();
+            length = str.Length - 1;
+
+            while (length >= 0)
+            {
+                reverse = reverse + str[length];
+                length--;
+            }
+            Console.WriteLine("Reverse word is {0}", reverse);
+            Console.ReadLine();
         }
     }
 }
